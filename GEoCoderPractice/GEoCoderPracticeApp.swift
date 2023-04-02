26f30316384768775fed5ee6acf8bf68094a11f0
Jacquese <jacqwhitson@gmail.com>
2023-04-02 15:03:08 -0400

    Initial Commit

---
+++ GEoCoderPractice/GEoCoderPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GEoCoderPracticeApp.swift
+//  GEoCoderPractice
+//
+//  Created by Jacquese Whitson  on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GEoCoderPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
